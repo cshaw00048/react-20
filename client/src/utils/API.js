a6d732e08cb9ed165a7489c1a4f3cb2e93edff06
Christian Shaw <cshaw00048@gmail.com>
2020-08-14 12:45:09 -0500

    finishing source folder javascript

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,20 +1,14 @@
 import axios from "axios";
+// endpoint from googlebooks api
+const BaseUrl = "https://www.googleapis.com/books/v1/volumes?q="
 
 export default {
- 
-  getBooks: function(q) {
-    return axios.get("/api/google", { params: { q: "title:" + q } });
-  },
-
-  getSavedBooks: function() {
-    return axios.get("/api/books");
-  },
-
-  deleteBook: function(id) {
-    return axios.delete("/api/books/" + id);
-  },
-
-  saveBook: function(bookData) {
-    return axios.post("/api/books", bookData);
-  }
+    // calls googlbooks api and retrieve books based on user input
+    searchBooks: (query) => axios.get(BaseUrl + query),
+    // get all books saved in db
+    getBooks: () => axios.get("/api/books"),
+    // saves a book to the db
+    saveBook: (bookData) => axios.post("/api/books", bookData),
+    // deletes a book with the given id
+    deleteBook: (id) => axios.delete("api/books/" + id)
 };
